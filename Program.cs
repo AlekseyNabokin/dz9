55984a70775b945bc5351744690b53aca6a320d5
Aleksey <nighttom@mail.ru>
2023-06-11 15:12:03 +0700

    zadacha 66

--- Program.cs
+++ Program.cs
@@ -3,14 +3,34 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-void ShowNumbers(int n)
+// void ShowNumbers(int n)
+// {
+//     if (n>0)
+//     {
+//         Console.Write(n + " ");
+//         ShowNumbers(n-1);
+//     }
+// }
+// Console.WriteLine("Input number N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// ShowNumbers(n);
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Amount(int m, int n)
 {
-    if (n>0)
-    {
-        Console.Write(n + " ");
-        ShowNumbers(n-1);
-    }
+    if (m==n)
+    return n;
+    else return m + Amount(m + 1, n);
 }
-Console.WriteLine("Input number N: ");
+
+Console.Write("Input number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-ShowNumbers(n);
+
+Console.Write($"The sum of natural elements in the range from M to N is equal to {Amount(m,n)}");
